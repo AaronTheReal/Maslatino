20024f3247d17e507b4cb06f65e0b20ca23eca5c
AaronTheReal <aaron@realmediagroups.com>
2025-06-23 22:00:47 -0600

    primer push

---
+++ backend/api/MainRoute.js
@@ -0,0 +1,34 @@
+import { fileURLToPath } from 'url';
+import multer from 'multer';
+import path from 'path';
+import MainController from './MainController.js';
+import SpotifyController from './SpotifyController.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '..', '..', 'frontend', 'src', 'assets', 'images'));
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+export default class MainRoute {
+  static configRoutes(router) {
+    // ✅ Usa rutas bien formadas
+    router.route('/').get(MainController.apiGetTests);
+    router.route('/search').get(SpotifyController.apiSearchPodcast);
+    router.route('/podcast').post(SpotifyController.savePodcast);
+    router.route('/podcasts').get(SpotifyController.getAllPodcasts);
+
+
+
+    return router;
+  }
+}
+
