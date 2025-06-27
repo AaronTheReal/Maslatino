68adaa14f888d250da5b716c02d957078006b682
AaronTheReal <aaron@realmediagroups.com>
2025-06-27 16:48:03 -0600

    íconos

--- mobile/src/app/pages/intro-tour/intro-tour.page.ts
+++ mobile/src/app/pages/intro-tour/intro-tour.page.ts
@@ -1,23 +1,44 @@
-import { Component, OnInit } from '@angular/core'; 
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+ import { CommonModule } from '@angular/common';
+ import { FormsModule } from '@angular/forms';
+ import { Router } from '@angular/router';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButton,
 
-@Component({
-  selector: 'app-intro-tour',
-  templateUrl: './intro-tour.page.html',
-  styleUrls: ['./intro-tour.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonButton, CommonModule, FormsModule],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
-})
-export class IntroTourPage implements OnInit {
+} from '@ionic/angular/standalone';
+ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+ 
+ 
+ @Component({
+   selector: 'app-intro-tour',
+   templateUrl: './intro-tour.page.html',
+   styleUrls: ['./intro-tour.page.scss'],
+   standalone: true,
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonButton,
 
+    CommonModule,
+    FormsModule,
+  ],
+   schemas: [CUSTOM_ELEMENTS_SCHEMA]
+ })
+ export class IntroTourPage implements OnInit {
 
-  ngOnInit() {}
+  constructor(private router: Router) {}
+ 
+   ngOnInit() {}
+ 
 
-
-}
+  proceed() {
+    this.router.navigate(['/login']);
+  }
+ }
